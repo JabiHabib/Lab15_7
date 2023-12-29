4dbdadf447147ed8c556a08a16f9609c53d74dc9
JabibHabib <kubusalsa@gmail.com>
2023-12-29 14:30:47 +0200

    Добавьте файлы проекта.

---
+++ Lab15_7/Lab15_7.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale>
+
+#define SIZE 10
+
+int main() {
+    setlocale(LC_ALL, "Ukr");
+    srand(time(NULL));
+
+    int x[SIZE];
+
+    for (int i = 0; i < SIZE; i++) {
+        x[i] = rand() % 100;
+    }
+
+    printf("Елементи масиву: ");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", x[i]);
+    }
+
+    int b = 0;
+
+    for (int i = 0; i < SIZE; i++) {
+        b += x[i];
+    }
+
+    printf("\nНакопичене значення: %d\n", b);
+
+    return 0;
+}
